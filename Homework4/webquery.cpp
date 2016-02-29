80ef94d42fba9069fdbe7578a52942b1efd999c8
Bert Peters <bert.ljpeters@gmail.com>
2016-02-29 12:16:42 +0100

    Implement multi-keyword search.

--- Homework4/webquery.cpp
+++ Homework4/webquery.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <queue>
 #include "IndexHelper.hpp"
 
 using namespace std;
@@ -41,23 +43,51 @@ map<string, int> getWeightedResults(const string& query)
 		entry.second += helper.getLinksToURL(entry.first);
 	}
 
-	// Filter out links that have not been crawled yet.
-	for (auto it = results.begin(); it != results.end(); ) {
-		if (!helper.hasCrawledURL(it->first)) {
-			cout << "Erased " << it->first << endl;
-			results.erase(it++);
-		} else {
-			++it;
-		}
-	}
-
 	return results;
 }
 
-vector<string> getResults(string query)
+vector<string> getResults(const string& query)
 {
+	stringstream tokens(query);
+	queue<map<string, int>> totalResults;
+	string token;
+	while (tokens >> token) {
+		if (token.empty()) {
+			continue;
+		}
+
+		totalResults.push(getWeightedResults(token));
+	}
+	if (totalResults.empty()) {
+		return {};
+	}
+
+	// Get the first keyword results
+	auto weightedResults = totalResults.front();
+	totalResults.pop();
+
+	while (!totalResults.empty()) {
+		auto newResults = totalResults.front();
+		totalResults.pop();
+		// Merge additional keyword results into them.
+		auto currentIt = weightedResults.begin();
+		auto newIt = newResults.begin();
+		while (currentIt != weightedResults.end() && newIt != newResults.end()) {
+			if (currentIt->first == newIt->first) {
+				currentIt->second += newIt->second;
+				currentIt++;
+				newIt++;
+			} else if (currentIt->first > newIt->first) {
+				newIt++;
+			} else {
+				weightedResults.erase(currentIt++);
+			}
+		}
+		weightedResults.erase(currentIt, weightedResults.end());
+	}
+
 	vector<pair<int, string>> orderedResults;
-	for (auto entry : getWeightedResults(query)) {
+	for (auto entry : weightedResults) {
 		orderedResults.emplace_back(entry.second, entry.first);
 	}
 
